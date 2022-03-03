b73a85b5b99299e652e622d55f6fce5ed7e3b3cd
Pedro Goulart <ph.goulart.almeida@gmail.com>
2022-03-03 18:21:33 -0300

    iniciando java

---
+++ eclipse-workspace/project_java/src/MetodosAbstratos/Company.java
@@ -0,0 +1,30 @@
+package MetodosAbstratos;
+
+public class Company extends TaxPayer {
+
+	private Integer numberOfEmployees;
+
+	public Company() {
+	}
+
+	public Company(String name, Double annualIncome, Integer numberOfEmployees) {
+		super(name, annualIncome);
+		this.numberOfEmployees = numberOfEmployees;
+	}
+
+	public Integer getNumberOfEmployees() {
+		return numberOfEmployees;
+	}
+
+	public void setNumberOfEmployees(Integer numberOfEmployees) {
+		this.numberOfEmployees = numberOfEmployees;
+	}
+
+	@Override
+	public double tax() {
+		double people = numberOfEmployees > 10 ? getAnnualIncome() * 0.16 : getAnnualIncome() * 0.14;
+
+		return people;
+	}
+
+}
